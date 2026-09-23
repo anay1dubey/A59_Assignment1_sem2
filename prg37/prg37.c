fdb6975c34ff920d4693c8fc6f280fed3f8d6e3a
Anay Dubey <151368294+anay1dubey@users.noreply.github.com>
2026-09-24 00:21:23 +0530

    Update prg37.c

--- prg37/prg37.c
+++ prg37/prg37.c
@@ -4,10 +4,6 @@ int main()
     int i,j;
     for(i=1;i<6;i++)
     {
-        for(j=0;j<(5-i);j++)
-        {
-            printf(" ");
-        }
         for(j=1;j<=i;j++)
         {
             printf("%d",j);
