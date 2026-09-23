73c1cd2cd8139e6627929810b9afea13f8a5a616
Anay Dubey <151368294+anay1dubey@users.noreply.github.com>
2026-09-24 00:24:53 +0530

    Refactor pattern printing logic in prg41.c

--- prg41/prg41.c
+++ prg41/prg41.c
@@ -4,10 +4,6 @@ int main()
     int i,j;
     for(i=1;i<=4;i++)
     {
-        for(j=1;j<=(4-i);j++)
-        {
-            printf(" ");
-        }
         for(j=1;j<=i;j++)
         {
             printf("%d",j);
